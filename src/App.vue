c2ac295d53277cebd274ddd2100e41450da338b5
Nico-186 <rnicolastabilo@gmail.com>
2024-06-10 00:55:47 -0400

    login first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <login>
+
+    </login>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import login from './components/Login.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        login
+    },
+    data() {
+        return {
+            theme: 'dark'
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="form-group" v-for="control in controls">
+        <label>{{control.text}}</label>
+        <input :type="control.type" class="form-control" :placeholder="control.placeholder">
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            controls: [
+                {
+                    text: 'Email address',
+                    type: 'email',
+                    placeholder: 'Enter email'
+                },
+                {
+                    text: 'Password',
+                    type: 'password',
+                    placeholder: 'Password'
+                }
+            ]
+        }
+    }
+}
+</script>
