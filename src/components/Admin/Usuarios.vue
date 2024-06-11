b881e7e86eefcabf0d61a3f62d7d230cc5aecddf
Nico-186 <rnicolastabilo@gmail.com>
2024-06-11 01:21:15 -0400

    Admin user managment table finished

--- src/components/Admin/Usuarios.vue
+++ src/components/Admin/Usuarios.vue
@@ -2,16 +2,13 @@
     <div class="container-fluid w-100 h-100" style="padding: 2%;">
         <div class="border w-100 h-100" style="background-color: #171a1c;">
             <div class="d-flex flex-column h-100">
-                <ul class="list-group">
-                    <li class="list-group-item border-0 bg-transparent">
-                        MIAU
-                    </li>
-                </ul>
-                <div class="flex-grow-1 overflow-auto">
-                    <div class="list-group h-100 w-100" style="max-height: 100%;">
+                <p class="w-100 mb-0 px-3 py-2 fs-5">Usuarios</p>
+
+                <div class="flex-grow-1 overflow-auto w-100">
+                    <div class="list-group h-100" style="max-height: 100%;">
                         <ul class="list-group">
                             <button v-for="(usuario, index) in usuarios" :id="index" type="button"
-                                class="list-group-item list-group-item-action" @click.prevent="">
+                                class="list-group-item list-group-item-action" :class="{ active: activeListItem == index}" @click.prevent="changeActiveListItem(index)">
                                 <div class="ms-2 me-auto">
                                     <div class="fw-bold">{{usuario.username}}</div>
                                     {{usuario.rol}}
@@ -19,6 +16,10 @@
                             </button>
                         </ul>
                     </div>
+                </div>
+
+                <div class="d-flex w-100 justify-content-end p-3">
+                    <button class="btn btn-secondary" type="button">{{ activeListItem == -1 ? 'Nuevo Usuario' : 'Editar Usuario' }}</button>
                 </div>
             </div>
         </div>
@@ -31,6 +32,7 @@
 export default {
     data() {
         return {
+            activeListItem: -1,
             usuarios:[{
                 username:'test',
                 rol: 'Tecnico'
@@ -46,7 +48,49 @@ export default {
             {
                 username:'LAURA',
                 rol: 'Usuario'
+            },{
+                username:'Nico.Miaus',
+                rol: 'Administrador'
+            },{
+                username:'Nico.Miaus',
+                rol: 'Administrador'
+            },{
+                username:'Nico.Miaus',
+                rol: 'Administrador'
+            },{
+                username:'Nico.Miaus',
+                rol: 'Administrador'
+            },{
+                username:'Nico.Miaus',
+                rol: 'Administrador'
+            },{
+                username:'Nico.Miaus',
+                rol: 'Administrador'
+            },{
+                username:'Nico.Miaus',
+                rol: 'Administrador'
+            },{
+                username:'Nico.Miaus',
+                rol: 'Administrador'
+            },{
+                username:'Nico.Miaus',
+                rol: 'Administrador'
+            },{
+                username:'Nico.Miaus',
+                rol: 'Administrador'
+            },{
+                username:'Nico.Miaus',
+                rol: 'Administrador'
             }]
+        }
+    },
+    methods: {
+        changeActiveListItem(index) {
+            if (this.activeListItem == index){
+                this.activeListItem = -1;
+            } else {
+                this.activeListItem = index;
+            };
         }
     }
 }
